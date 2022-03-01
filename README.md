89e8b6c796200f510e7ee835281efb751102039d
dcaro1996 <dcaro1996@yahoo.com>
2022-03-01 11:47:31 -0500

    page v1

--- README.md
+++ README.md
@@ -1,9 +1,29 @@
 # day-planner
-This is a homework assignment for UCF Coding Bootcamp
-<p> Incomplete for the time-being, will work to complete as soon as possible! </p>
+## 👨‍💻 Mission
+Design a tool to increase productivity in users by helping them plan for their day.
 
-<Link to the live webpage will go here link rel=webpage src>
+## 🌐 Application
+<a href="https://pr1me-e1gh7.github.io/weather-dashboard/">Link to the App</a>
 
-Description will go here
-
-<img will go here>
+## 💭 Description
+```md
+WORDS WILL GO HERE
+```
+## 🏁 Challenge Criteria
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+## 🎨 Preview
+<img src="/assets/images/day-planner-preview.gif"/>
---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+// Live clock
+window.setInterval(function () {
+    $('#clock').html(moment().format('[Today\'s Date: ]YYYY-MM-DD HH:mm:ss'))
+}, 1000);
+
+// BUTTONS
+// Adds function to Save buttons
+$('.btnSave').on('click', function () {
+    let textArea = $(this).siblings('textarea').val();
+    let rowIndex = $(this).parent().data('index');
+    let textValue = textArea;
+
+    localStorage.setItem('row ' + rowIndex, JSON.stringify(textValue));
+})
+// Adds function to Clear button
+$('.btnClear').on('click', function () {
+    localStorage.clear();
+    location.reload();
+})
+
+// PLANNER
+// Renders the planner boxes
+$(function txtArea() {
+    let timeRow = $('.row');
+    let timeNow = Number(moment().format('H'));
+
+    timeRow.each(function (i) {
+        let timeRowText = $(this).children('textarea');
+        let timeRowStore = JSON.parse(localStorage.getItem('row ' + i))
+
+    // Changes box color dependent on time
+    if (timeNow === $(this).data('time')) {
+        $(this).children('textarea').addClass('timeNow');
+        $(this).prevAll().children('textarea').addClass('timeBefore');
+        $(this).nextAll().children('textarea').addClass('timeAfter');
+    }
+    // Checks for saved plans
+    if (timeRowStore === null) {
+        return;
+    }
+    // Sets saved plans
+        timeRowText.val(timeRowStore);
+    });
+
+    // Red if before 8 AM
+    if (timeNow < 8) {
+        $('.container').find('textarea').addClass('timeAfter');
+    // Blue if after 8 PM
+    } else if (timeNow > 20) {
+        $('.container').find('textarea').addClass('timeBefore');
+    }
+});
